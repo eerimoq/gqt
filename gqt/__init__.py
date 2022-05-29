33453e565084fdac088b73141251dc3d47f37aca
Erik Moqvist <erik.moqvist@gmail.com>
2022-05-29 16:21:13 +0200

    Continued.

--- gqt/__init__.py
+++ gqt/__init__.py
@@ -1,14 +1,1 @@
-import sys
-import time
-
-from rich.console import Console
-from rich.progress import Progress
-
-def main():
-    with Progress(console=Console(file=sys.stderr)) as progress:
-        task = progress.add_task("Test", total=2)
-        time.sleep(0.5)
-        progress.update(task, advance=1)
-        time.sleep(0.5)
-        progress.update(task, advance=1)
-        time.sleep(0.5)
+# todo
